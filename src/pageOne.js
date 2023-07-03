05d818bc3b43059e7226315520a007f45b2d01cf
norristown <jarvasity@gmail.com>
2023-07-03 01:13:15 -0400

    added hot dog picture

--- src/pageOne.js
+++ src/pageOne.js
@@ -1,22 +1,29 @@
 function pageOne() {
-    const review = {
+    const page = {
         blurb:
             `"Hot Dog Heaven is the best in hot dog flavor! 
             I come by at least three times a week and I would actually come more, 
             but I must exhbit some kind of self-control! HOT DOG HEAVEN NUMBER ONE!"`,
         author: `-Spot`,
-        pic: '/src/dog_quote.jpg'
+        pic: '/src/dog_quote.jpg',
+        mascot: '/src/cute_hotdog.jpg'
     }
 
     const content = document.querySelector('.content');
     const container = document.createElement('div');
     container.classList.add('container')
-    container.textContent = 'Hot Dog Heaven';
+    const hotDogImg = document.createElement('img');
+    hotDogImg.classList.add('cute-hotdog');
+    hotDogImg.src = page.mascot;
+    hotDogImg.height = 300;
+    hotDogImg.width = 300;
+    container.textContent = `Hot Dog Heaven`;
+    content.appendChild(hotDogImg)
     content.appendChild(container)
 
     const description = document.createElement('div');
     description.classList.add('description')
-    description.textContent = review.blurb
+    description.textContent = page.blurb
     content.appendChild(description)
 
     console.log(review.blurb)
